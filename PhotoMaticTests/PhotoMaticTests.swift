a30653f83baae1b474f590e29bff22ac2546edc5
NirTurjeman <nirturjeman@gmail.com>
2025-04-18 12:44:41 +0300

    Initial Commit

---
+++ PhotoMaticTests/PhotoMaticTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMaticTests.swift
+//  PhotoMaticTests
+//
+//  Created by NirTurjeman on 18/04/2025.
+//
+
+import Testing
+@testable import PhotoMatic
+
+struct PhotoMaticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
